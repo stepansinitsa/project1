c8222b5c167356ac13156eb2c4ca975093a04172
stepan.sinitsa <stepansinitsa@gmail.com>
2024-08-14 13:16:31 +0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './css/style.css';
+import './js/app';
+import './js/basic';
+import './js/http';
+import './js/user';
+import './js/__tests__/basic.test';
+import './js/__tests__/user.test';
